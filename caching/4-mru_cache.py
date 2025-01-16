d5e20409150690bcdf8270717bf4537761081428
Chidera0001 <c.anele@alustudent.com>
2025-01-16 10:07:30 +0000

    Hopefully this works

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python3
-""" lru cache """
 
-BaseCaching = __import__('base_caching').BaseCaching
+'''MRUCache module'''
+
+
+from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ lru cache """
+    """ MRUCache inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """ Initialize MRUCache
+        """
+        super().__init__()
+        self.access_order = []
 
     def put(self, key, item):
-        """Add an item in the cache"""
-        if key is None or item is None:
-            return
-        if (key not in self.cache_data and
-                len(self.cache_data) >= BaseCaching.MAX_ITEMS):
-            if self.cache_data:
-                discard = next(reversed(list(self.cache_data)))
-                print("DISCARD: {}".format(discard))
-                del self.cache_data[discard]
-        if key in self.cache_data:
-            del self.cache_data[key]
-        self.cache_data[key] = item
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.access_order.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                mru_key = self.access_order.pop()
+                del self.cache_data[mru_key]
+                print("DISCARD: {}".format(mru_key))
+            self.cache_data[key] = item
+            self.access_order.insert(0, key)
 
     def get(self, key):
-        """Get an item in the cache"""
-        if key is None or key not in self.cache_data:
-            return None
-        item = self.cache_data[key]
-        del self.cache_data[key]
-        self.cache_data[key] = item
-        return self.cache_data[key]
+        """ Get an item by key
+        """
+        if key is not None:
+            if key in self.cache_data:
+                self.access_order.remove(key)
+                self.access_order.insert(0, key)
+                return self.cache_data[key]
+            else:
+                return None
